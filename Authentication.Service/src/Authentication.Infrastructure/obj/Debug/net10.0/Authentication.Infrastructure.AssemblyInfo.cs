16feedcce572a27dcecac4f93b555a1a01e5f464
Islam Chaachaa <imisikchaachaa@gmail.com>
2026-07-13 13:53:53 +0100

    test css

--- Authentication.Service/src/Authentication.Infrastructure/obj/Debug/net10.0/Authentication.Infrastructure.AssemblyInfo.cs
+++ Authentication.Service/src/Authentication.Infrastructure/obj/Debug/net10.0/Authentication.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Authentication.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b917ee260a54ec9b9a838486738fe3de7aa915")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3415594e3610baa8c10a4241aa1f5c63b8dacb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Authentication.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Authentication.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
