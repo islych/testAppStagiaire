924b6c95f589e19e682b69ac63d49f94672e84b9
nassima05-amoussi <nassimaams05@gmail.com>
2026-07-20 21:24:55 +0100

    feat(documents): add migration AjoutDestinataireActuel

--- Documents.Service/src/Documents.Infrastructure/Persistence/Migrations/20260720202334_AjoutDestinataireActuel.Designer.cs
+++ Documents.Service/src/Documents.Infrastructure/Persistence/Migrations/20260720202334_AjoutDestinataireActuel.Designer.cs
@@ -4,6 +4,7 @@ using Documents.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Documents.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(DocumentsDbContext))]
-    partial class DocumentsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260720202334_AjoutDestinataireActuel")]
+    partial class AjoutDestinataireActuel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Documents.Service/src/Documents.Infrastructure/Persistence/Migrations/20260720202334_AjoutDestinataireActuel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Documents.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjoutDestinataireActuel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
